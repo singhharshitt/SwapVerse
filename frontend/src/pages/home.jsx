21dca0288e8e3af88320eef17bff9d18bf998a4b
singhharshitt <harshitsinghrajput242004@gmail.com>
2025-07-27 12:48:33 +0530

    Added swapping form and footer

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,67 +1,186 @@
-import { useState, useEffect } from 'react'; 
-import history from '../assets/history.png';
+import { useState, useEffect } from "react";
+import history from "../assets/history.png";
 import { Link } from "react-router-dom";
+import swapp from "../assets/swapp.png";
 
 export default function Home() {
-    return (
-        <>
-            {/* Navbar */}
-            <nav className="flex items-center justify-between sticky top-0 z-50 bg-[#5B2333] p-4 md:p-8 border-b border-gray-800">
-                <div className="flex items-center">
-                    <h2 className="text-2xl md:text-4xl text-[#F7F4F3]" style={{ fontFamily: '"Pacifico", cursive' }}>
-                        SwapVerse
-                    </h2>
+  const tokens = ["TKA7", "TKB7"];
+  const [tokenA, setTokenA] = useState("");
+  const [tokenB, setTokenB] = useState("");
+  const handleTokenA = (e) => {
+    const sel = e.target.value;
+    setTokenA(sel);
+    if (sel == tokenB) {
+      setTokenB("");
+    }
+  };
+
+  const handleTokenB = (e) => {
+    const sel = e.target.value;
+    setTokenB(sel);
+    if (sel == tokenA) {
+      setTokenA("");
+    }
+  };
+  return (
+    <>
+      {/* Navbar */}
+      <nav className="flex items-center justify-between sticky top-0 z-50 bg-[#5B2333] p-4 md:p-8 border-b border-gray-800">
+        <div className="flex items-center">
+          <h2
+            className="text-2xl md:text-4xl text-[#F7F4F3]"
+            style={{ fontFamily: '"Pacifico", cursive' }}
+          >
+            SwapVerse
+          </h2>
+        </div>
+        <div className="flex items-center space-x-4">
+          <Link
+            to="/historyy"
+            className="p-2 hover:bg-gray-800 rounded-lg transition-colors"
+          >
+            <img
+              src={history}
+              alt="history"
+              className="w-6 h-6 md:w-8 md:h-8"
+            />
+          </Link>
+          <button className="border-2 border-white bg-[#F24333] px-4 py-2 rounded-xl hover:bg-[#BA1B1D] transition-colors">
+            <span
+              className="text-sm md:text-md text-white font-semibold"
+              style={{ fontFamily: '"Roboto", sans-serif' }}
+            >
+              Connect Wallet
+            </span>
+          </button>
+        </div>
+      </nav>
+
+      {/* TOKEN SWAP FORM */}
+      <div className="min-h-screen bg-[#F7F4F3] p-4 md:p-8">
+        <div className="max-w-4xl mx-auto">
+          <h1
+            className="text-3xl md:text-5xl text-[#5B2333] text-center mt-8 mb-6 font-extrabold"
+            style={{ fontFamily: '"Bitter", serif' }}
+          >
+            Welcome To{" "}
+            <span
+              className="text-[#F24333]"
+              style={{ fontFamily: '"Pacifico", cursive' }}
+            >
+              SwapVerse
+            </span>
+          </h1>
+          <p className="text-[#564D4A] text-center mb-18 text-sm">
+            <i>Your trusted platform for seamless token swapping</i>
+          </p>
+
+          <div className="bg-[#332F2F] rounded-xl p-8 border border-gray-700 max-w-xl mx-auto">
+            <form style={{ fontFamily: '"Montserrat", sans-serif' }}>
+              {/* Selling Section */}
+              <div className=" bg-[#F7F4F3] rounded-lg p-4 mt-4">
+                <label
+                  className="block text-[#5B2333] text-xs mb-2 font-semibold text-[10px]"
+                  style={{ fontFamily: '"Montserrat", sans-serif' }}
+                >
+                  You're Selling
+                </label>
+                <div className="flex justify-between items-center">
+                  <input
+                    type="number"
+                    placeholder="0.00"
+                    className="w-1/2 p-2  text-sm focus:outline-none bg-[#F7F4F3] no-spinner"
+                  />
+                  <select
+                    className="w-1/3 p-2 rounded-md border border-gray-300 text-sm bg-white"
+                    onChange={(e) => handleTokenA(e)}
+                    value={tokenA}
+                  >
+                    <option value="">-- Select Token --</option>
+                    {tokens.map((token) =>
+                      token !== tokenB ? (
+                        <option key={token} value={token}>
+                          {token}
+                        </option>
+                      ) : null
+                    )}
+                  </select>
                 </div>
-                <div className="flex items-center space-x-4">
-                    <Link to="/historyy" className="p-2 hover:bg-gray-800 rounded-lg transition-colors">
-                        <img src={history} alt="history" className="w-6 h-6 md:w-8 md:h-8" />
-                    </Link>
-                    <button className="border-2 border-white bg-[#F24333] px-4 py-2 rounded-xl hover:bg-[#BA1B1D] transition-colors">
-                        <span className="text-sm md:text-md text-white font-semibold" style={{ fontFamily: '"Roboto", sans-serif' }}>
-                            Connect Wallet
-                        </span>
-                    </button>
+              </div>
+
+              {/* Swap icon */}
+              <div className="flex justify-center items-center">
+                <div
+                  className="rounded-full bg-[#0D0D0D] p-2 w-fit cursor-pointer hover:"
+                  id=""
+                >
+                  <img
+                    src={swapp}
+                    alt="swap icon"
+                    className="w-[28px] h-[28px]"
+                  />
                 </div>
-            </nav>
+              </div>
 
-            {/* TOKEN SWAP FORM */}
-            <div className="min-h-screen bg-[#F7F4F3] p-4 md:p-8">
-                <div className="max-w-4xl mx-auto">
-                    <h1 className="text-3xl md:text-5xl text-[#5B2333] text-center mt-8 mb-6 font-extrabold" style={{ fontFamily: '"Bitter", serif' }}>
-                        Welcome To <span className='text-[#F24333]' style={{ fontFamily: '"Pacifico", cursive' }}>SwapVerse</span>
-                    </h1>
-                    <p className="text-[#564D4A] text-center mb-18 text-lg">
-                        <i>Your trusted platform for seamless token swapping</i>
-                    </p>
-                    
-                    
-                    <div className="bg-[#564D4A] rounded-xl p-8 border border-gray-700 flex justify-between">
-                        <form className=''>
-                            <div className='m-10 w-180 h-38 border bg-[#F7F4F3] rounded-lg'>
-                            <label className='top-0 left-5 text-sm md:text-md text-[#5B2333] mb-5 w-full' style={{ fontFamily: '"Lobster Two", cursive' }}>You're Selling</label>
-                            <input className='w-60 h-22' type='' >
-
-                                
-                            </input>
-                            <input className=''>
-                                </input>
-
-                            </div>
-                            <div className='relative m-10 w-180 h-38 border bg-[#F7F4F3] rounded-lg'>
-                                <label className='absolute top-0 left-5 text-[#5B2333] text-sm md:text-md' style={{ fontFamily: '"Lobster Two", cursive' }}>To Buy</label>
-                            </div>
-                            <div className='m-10 flex justify-between items-center'>
-                                <button className='bg-[#F24333] hover:bg-[#BA1B1D] p-8 border rounded-xl'><span>Swap</span></button>
-                            </div>
-
-                        </form>
-                        
-                    </div>
+              {/* Buying Section */}
+              <div className="mb-6 bg-[#F7F4F3] rounded-lg p-4 ">
+                <label
+                  className="block text-[#5B2333] text-[10px] mb-2 font-semibold"
+                  style={{ fontFamily: '"Montserrat", sans-serif' }}
+                >
+                  To Buy
+                </label>
+                <div className="flex justify-between items-center">
+                  <input
+                    type="number"
+                    placeholder="0.00"
+                    className="w-1/2 p-2  text-sm focus:outline-none no-spinner"
+                  />
+                  <select
+                    className="w-1/3 p-2 rounded-md border border-gray-300 text-sm bg-white"
+                    onChange={(e) => handleTokenB(e)}
+                    value={tokenB}
+                  >
+                    <option value="">-- Select Token --</option>
+                    {tokens.map((token) =>
+                      token !== tokenA ? (
+                        <option key={token} value={token}>
+                          {token}
+                        </option>
+                      ) : null
+                    )}
+                  </select>
                 </div>
-            </div>
-            <footer>
+              </div>
 
-            </footer>
-        </>
-    );
+              {/* Swap Button */}
+              <div className="flex justify-center">
+                <button
+                  type="submit"
+                  className="bg-[#F24333] hover:bg-[#BA1B1D] text-white px-6 py-2 rounded-xl font-semibold"
+                >
+                  Swap
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <footer className="bg-[#5B2333] w-full" style={{ fontFamily: '"Libertinus Mono", monospace' }}>
+        <div className="flex">
+          <div className="flex items-center justify-center w-full">
+            <h2 className="text-lg font-bold text-[#F7F4F3] m-4" >Designed By Harshit Singh</h2>
+          </div>
+          <div></div>
+        </div>
+        <hr className="m-4"/>
+        <div className="flex justify-center items-center w-full">
+        <p className="text-[12px] text-[#F7F4F3] ">
+          © 2025 SwapVerse. All rights reserved. Empowering seamless token swaps
+          with security and speed.
+        </p>
+        </div>
+      </footer>
+    </>
+  );
 }
